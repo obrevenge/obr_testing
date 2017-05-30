ca34a79791db835c5bcdb3390c34f3f69fc1a6c0
obrevenge <ajjames31@gmail.com>
2017-05-30 17:05:28 -0500

    updated nemisis

--- working/nemesis/nemisis.sh
+++ working/nemesis/nemisis.sh
@@ -2,17 +2,14 @@
 
 title="Nemesis Installer"
 
-yad --plug=12345 --tabnum=1 --text="<big>Welcome to the Nemesis Installer</big>\nThank you for Choosing a Revenge OS System" --form --field=" ":LBL --image="obr-logo-sm.png" --image-on-top --field="Click on the Next Tab to Get Started":LBL > /dev/null &
+yad --plug=12346 --tabnum=1 --text="<big>Welcome to the Nemesis Installer</big>\nThank you for Choosing a Revenge OS System" --form --field=" ":LBL --image="obr-logo-sm.png" --image-on-top --field="Click on the Next Tab to Get Started":LBL > /dev/null &
 
                                                    
-yad --plug=12345 --tabnum=2 --text="Partitions" --text="<big>Partitioning</big>\n\nHow Would You Like to Partition Your Disk?\nAuto Partitioning will delete everything\non the disk and install Revenge OS\n\nManual Partitioning will allow you\nto create and edit partitions and choose where\nto install Revenge OS" --image="obr-logo-sm.png" --image-on-top --radiolist --list --column="Select" --column="Type" false "Automatic Partioning" false "Manual Partitioning" --separator=" " > answer.txt &     
+yad --plug=12346 --tabnum=2 --text="Partitions" --text="<big>Partitioning</big>\n\nHow Would You Like to Partition Your Disk?\nAuto Partitioning will delete everything\non the disk and install Revenge OS\n\nManual Partitioning will allow you\nto create and edit partitions and choose where\nto install Revenge OS" --image="obr-logo-sm.png" --image-on-top --radiolist --list --column="Select" --column="Type" false "Automatic Partioning" false "Manual Partitioning" --separator=" " > answer.txt &    
 
-part=` cat answer.txt | awk '{print $2}' `
 
-cat answer.txt | awk '{print $2}'
+yad --width=600 --height=500 --title="$title" --notebook --key=12346 --tab="Welcome" --tab="Tab 2"
 
-rm answer.txt
-
-yad --width=600 --height=500 --title="$title" --notebook --key=12345 --tab="Welcome" --tab="Tab 2"
+part=` cat answer.txt | awk '{print $2;}' `
 
 echo "The choice is $part"
